7374326481ae1be6b0a8d2735f000aa74f15b555
xh24 <samuel.schnoedt@web.de>
2026-04-13 14:08:21 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,9 @@
-const config = {
-  apiKey: "DEIN_KEY",
-  databaseURL: "DEINE_URL"
+const firebaseConfig = {
+  apiKey: "AIzaSyAa4Uk3cHjZWVcRBbNsi3rYYoW-WGXQub4",
+  authDomain: "squidchat-chatrooms-ea3cf.firebaseapp.com",
+  databaseURL: "https://squidchat-chatrooms-ea3cf-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "squidchat-chatrooms-ea3cf"
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
