46f37861c73154a5b9c61cf1a36c2842b55a5c49
xh24 <samuel.schnoedt@web.de>
2026-04-13 14:26:26 +0200

    Update ui.js

--- ui.js
+++ ui.js
@@ -4,10 +4,7 @@ function addMessage(msg) {
   const el = document.createElement("div");
   el.className = "message";
 
-  el.innerHTML = `
-    <b>${msg.user}</b><br>
-    ${msg.text}
-  `;
+  el.innerHTML = `<b>${msg.user}</b><br>${msg.text}`;
 
   chat.appendChild(el);
   chat.scrollTop = chat.scrollHeight;
