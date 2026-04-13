fb6f9dd20056dc135c0c8d243945853cf9e42c73
xh24 <samuel.schnoedt@web.de>
2026-04-13 14:25:45 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onChildAdded
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAa4Uk3cHjZWVcRBbNsi3rYYoW-WGXQub4",
   authDomain: "squidchat-chatrooms-ea3cf.firebaseapp.com",
+  projectId: "squidchat-chatrooms-ea3cf",
   databaseURL: "https://squidchat-chatrooms-ea3cf-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "squidchat-chatrooms-ea3cf"
-  storageBucket: "squidchat-chatrooms-ea3cf.firebasestorage.app",
-  messagingSenderId: "149200968301",
-  appId: "1:149200968301:web:2f4ca325229d23c6f53d64"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.fb = {
+  db,
+  ref,
+  push,
+  onChildAdded
+};
