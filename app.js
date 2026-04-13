5c98544caec565293c2151a03a43d80dac608ebc
xh24 <samuel.schnoedt@web.de>
2026-04-13 14:11:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 let currentRoom = "lobby";
 
+// Raum wechseln
 function switchRoom(room) {
   currentRoom = room;
   document.getElementById("chat").innerHTML = "";
 }
 
+// Nachricht senden
 function send() {
   const user = document.getElementById("username").value || "Inkling";
   const text = document.getElementById("msg").value;
 
+  if (!text) return;
+
   db.ref("messages").push({
-    room: currentRoom,
     user,
     text,
+    room: currentRoom,
     time: Date.now()
   });
 
   document.getElementById("msg").value = "";
 }
 
+// Nachrichten laden
 db.ref("messages").on("child_added", snap => {
   const msg = snap.val();
 
